2c32c01bc265d74d8295b60cab410ba09910958a
PLSV <greasemonkey12394@gmail.com>
2018-05-06 00:21:30 +0530

    setup email functionality for sign up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
+  	#SENDGRID API KEY : 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
   end
 end
