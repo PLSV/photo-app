6d45ff84152b4f443751570dc74f051789e09ff3
PLSV <greasemonkey12394@gmail.com>
2018-05-06 09:43:51 +0530

    small change

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,5 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-  	#SENDGRID API KEY : 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
   end
 end
